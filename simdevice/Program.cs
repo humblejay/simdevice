32040c198b4eb99e62f28cc2c821c849d08e7db0
humblejay <humblej@outlook.com>
2021-02-18 20:10:05 -0800

    Add project files.

---
+++ simdevice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace simdevice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+        }
+    }
+}
