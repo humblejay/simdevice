83364c2bea05be1eac6db619293ad1093b9b04ef
humblejay <humblej@outlook.com>
2021-02-19 09:35:03 -0800

    added provisioning

--- simdevice/Program.cs
+++ simdevice/Program.cs
@@ -3,24 +3,62 @@ using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.DependencyInjection;
+using CommandLine;
+using Microsoft.Azure.Devices.Client;
+using System.Threading.Tasks;
 
 
 namespace simdevice
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
+            //Get Configuration
+            var setConfig = GetConfiguration(args);
 
+
+            // Parse application parameters
+            Parameters parameters = null;
+            ParserResult<Parameters> result = Parser.Default.ParseArguments<Parameters>(args)
+                .WithParsed(parsedParams =>
+                {
+                    parameters = parsedParams;
+                })
+                .WithNotParsed(errors =>
+                {
+                    Environment.Exit(1);
+                });
+
+        
+
+            var sample = new ProvisioningDeviceClientSample(parameters);
+            await sample.RunSampleAsync();
+
+            Console.WriteLine("Enter any key to exit.");
+            Console.ReadKey();
+
+            return 0;
+
+
+         
+
+        
+        }
+
+        static IConfiguration GetConfiguration(string[] args)
+        {
             ConfigurationBuilder builder = new ConfigurationBuilder();
             builder
+                 .SetBasePath(Environment.CurrentDirectory)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .SetBasePath(Environment.CurrentDirectory)
                 .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
                 .AddEnvironmentVariables()
-                .AddCommandLine(args)
-                .Build();
+                .AddCommandLine(args);
+                
 
+            var setConfig= builder.Build();
+            return setConfig;
         }
     }
 }
---
+++ simdevice/Readme.txt
@@ -0,0 +1,35 @@
+﻿Following are configuration parameters for this program:
+
+Add RegistrationPrefix & DTDL modelId for the device in appsettings.json file as below
+---------------------------------------------------------------------------
+{
+  "exclude": [
+    "**/bin",
+    "**/bower_components",
+    "**/jspm_packages",
+    "**/node_modules",
+    "**/obj",
+    "**/platforms"
+  ],
+
+  "RegistrationPrefix": "demodevice_",   
+  "ModelId": "dtmi:azurertos:devkit:gsgmxchip;1"
+}
+
+For local development & testing
+Add this in secrets.json file (right click project > Manage User Secrets)
+----------------------------------------
+{
+  "DpsIdScope": "",
+  "DpsGroupPrimaryKey": "",
+  "DpsGroupSecondaryKey": "",
+
+  "FileUploadBlob": "",
+  "FileDownloadBlob": "",
+
+ }
+
+ For production 
+    Option 1: Copy secrets.json content into appsettings.json  OR
+    Option 2: Configure them as Environment variables  OR
+    Option 3: Pass them as command line parameters
